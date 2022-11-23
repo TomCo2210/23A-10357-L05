0cf7ce26623c929ec713f4143b256f92928a6f2f
Tom Cohen <Tomco@outlook.co.il>
2022-11-23 15:49:12 +0200

    JSON, GSON, MySPV1

--- app/src/main/java/dev/tomco/a23a_10357_l05/MainActivity.java
+++ app/src/main/java/dev/tomco/a23a_10357_l05/MainActivity.java
@@ -3,6 +3,14 @@ package dev.tomco.a23a_10357_l05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
+
+import dev.tomco.a23a_10357_l05.Model.Playlist;
+import dev.tomco.a23a_10357_l05.Model.Song;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Playlist playlist = new Playlist();
+        playlist.setName("Top 50");
+
+        playlist.getSongs().add(new Song()
+                .setName("פנתרה")
+                .setArtist("נועה קירל")
+                .setDuration(185)
+                .setReleaseDate(System.currentTimeMillis()/1000)
+                .setRating(4.3f)
+                .setViews(18001491)
+                .addTag("Pop")
+                .addTag("Israeli")
+        );
+
+        playlist.getSongs().add(new Song()
+                .setName("Gangnam Style")
+                .setArtist("PSY")
+                .setDuration(252)
+                .setReleaseDate(System.currentTimeMillis()/1000)
+                .setRating(4.9f)
+                .setViews(4_060_000_000l)
+                .addTag("K-Pop")
+                .addTag("Korean")
+        );
+
+
+        String json = new Gson().toJson(playlist);
+        Log.d("JSON", json);
+
+        String json2 = "{\"name\":\"Top 50\",\"songs\":[{\"artist\":\"נועה קירל\",\"duration\":185,\"name\":\"פנתרה\",\"rating\":4.3,\"releaseDate\":1669208739,\"tags\":[\"Pop\",\"Israeli\"],\"views\":18001491},{\"artist\":\"PSY\",\"duration\":252,\"name\":\"Gangnam Style\",\"rating\":4.9,\"releaseDate\":1669208739,\"tags\":[\"K-Pop\",\"Korean\"],\"views\":4060000000}]}";
+        Playlist playlist2 = new Gson().fromJson(json2,Playlist.class);
+        Log.d("Playlist From JSON", playlist2.toString());
     }
 }
---
+++ app/src/main/java/dev/tomco/a23a_10357_l05/Utils/MySPv1.java
@@ -0,0 +1,32 @@
+package dev.tomco.a23a_10357_l05.Utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MySPv1 {
+    private static final String DB_FILE = "DB_FILE";
+
+    public static void putInt(Context context, String key, int value) {
+        SharedPreferences preferences = context.getSharedPreferences(DB_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(key, value);
+        editor.commit();
+    }
+
+    public static int getInt(Context context, String key, int defValue) {
+        SharedPreferences preferences = context.getSharedPreferences(DB_FILE, Context.MODE_PRIVATE);
+        return preferences.getInt(key, defValue);
+    }
+
+    public static void putString(Context context, String key, String value) {
+        SharedPreferences preferences = context.getSharedPreferences(DB_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public static String getString(Context context, String key, String defValue) {
+        SharedPreferences preferences = context.getSharedPreferences(DB_FILE, Context.MODE_PRIVATE);
+        return preferences.getString(key, defValue);
+    }
+}
